15839799e021d715fda969930e241954716f5ef3
Zapa-doti <84052230+Zapa-doti@users.noreply.github.com>
2021-06-11 11:35:22 -0500

    My first commit

---
+++ SimpleCalculator.js
@@ -0,0 +1,15 @@
+var number1;
+var number2;
+
+var number1 = parseFloat(prompt("Introduce el primer número"));
+var number2 = parseFloat(prompt("Introduce el segundo número"));
+
+let sum = number1 + number2;
+let subtraction = number1 - number2;
+let multiplication = number1 * number2;
+let division = number1 / number2;
+
+prompt("The sum is: " + sum);
+prompt("The subtraction is: " + subtraction);
+prompt("The multiplication is: " + multiplication);
+prompt("The division is: " + division);
